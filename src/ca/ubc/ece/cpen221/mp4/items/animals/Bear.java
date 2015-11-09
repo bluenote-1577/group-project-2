fd9615a0ebe8cfac36fda386972b927d96213e0a
Sean MacDonald <sean@gmail.com>
2015-11-08 23:05:15 -0800

    Bear File almost complete

--- src/ca/ubc/ece/cpen221/mp4/items/animals/Bear.java
+++ src/ca/ubc/ece/cpen221/mp4/items/animals/Bear.java
@@ -1,14 +1,43 @@
 package ca.ubc.ece.cpen221.mp4.items.animals;
 
+import javax.swing.ImageIcon;
+
 import ca.ubc.ece.cpen221.mp4.Location;
+import ca.ubc.ece.cpen221.mp4.Util;
+import ca.ubc.ece.cpen221.mp4.ai.AI;
 import ca.ubc.ece.cpen221.mp4.items.LivingItem;
 
 public class Bear extends AbstractArenaAnimal {
+	
+	private static final ImageIcon bearImage = Util.loadImage("bear.gif");
+	
+	//bear constructor 
+	public Bear(AI bearAI, Location initial_location){
+	 	
+		this.setImage(bearImage); 
+		
+		
+		
+		//initialize all the protected methods from AbstractArenaAnimal 
+		setINITIAL_ENERGY(100);
+		//setEnergy(90);
+		setMAX_ENERGY(150);
+		setSTRENGTH(200);
+		setVIEW_RANGE(1);
+		setMIN_BREEDING_ENERGY(30);
+		setCOOLDOWN(4); 
+		setLocation(initial_location); 
+		
+		
+	}
 
 	@Override
 	public LivingItem breed() {
 		// TODO Auto-generated method stub
-		return null;
+		Bear child = new Bear(getAi(), getLocation()); 
+		child.setEnergy(getEnergy() / 2);
+		this.setEnergy(getEnergy() / 2);
+		return child;
 	}
 
 	@Override
@@ -16,38 +45,4 @@ public class Bear extends AbstractArenaAnimal {
 		// TODO Auto-generated method stub
 		return "Bear";
 	}
-
-	
-	AbstractArenaAnimal.setINITIAL_ENERGY(100);
-	
-
-	protected void setEnergy(int i) {
-		this.energy = i;
-	}
-
-	protected void setMAX_ENERGY(int i) {
-		this.MAX_ENERGY = i;
-	}
-
-	protected void setSTRENGTH(int i) {
-		this.STRENGTH = i;
-	}
-
-	protected void setVIEW_RANGE(int i) {
-		this.VIEW_RANGE = i;
-	}
-
-	protected void setMIN_BREEDING_ENERGY(int i) {
-		this.MIN_BREEDING_ENERGY = i;
-	}
-
-	protected void setCOOLDOWN(int i) {
-		this.COOLDOWN = i;
-	}
-
-	protected void setLocation(Location l) {
-		this.location = l;
-	}
-	
-
 }
